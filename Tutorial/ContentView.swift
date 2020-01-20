695992d6a4a1db8f9b8e071df84e59b7d0356768
jw <betrayers000@gmail.com>
2020-01-20 22:51:17 +0900

    start swift tutorial & basic tutorial

---
+++ Tutorial/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Tutorial
+//
+//  Created by 박정우 on 16/01/2020.
+//  Copyright © 2020 박정우. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // struct value can't change in inner method
+    // struct value can change using @state
+    @State var touchedCount = 0
+    @State var name = ""
+    @State var age = ""
+    
+    var body: some View {
+        NavigationView{
+            Form{
+                Text("Hello, World! click count:\(touchedCount)")
+                Button("Button"){
+                    
+                    // button click event callback
+                    self.touchedCount += 1
+                }
+                Section{
+                    // TextField use for input string, text attr bind value
+                    TextField("input name", text: $name)
+                    Text("My name is \(name)")
+                }
+                
+                Section{
+                    // Using keyboardType for select keyboardType eg.numberkeypad
+                    TextField("input your age", text: $age)
+                        .keyboardType(.numberPad)
+                }
+                Section{
+                    Button("SignUp"){
+                        
+                    }
+                }
+            }.navigationBarTitle("This is Title")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tutorial/SignUp.swift
@@ -0,0 +1,21 @@
+//
+//  SignUp.swift
+//  Tutorial
+//
+//  Created by 박정우 on 2020/01/20.
+//  Copyright © 2020 박정우. All rights reserved.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignUp_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUp()
+    }
+}
